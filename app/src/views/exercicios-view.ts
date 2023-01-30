2ec054a44b628ca5c251cd6a53cc1c93495dd9fe
luiizsilverio <luiiz.silverio@gmail.com>
2023-01-29 21:09:59 -0300

    layout da tela de exercícios

--- app/src/views/exercicios-view.ts
+++ app/src/views/exercicios-view.ts
@@ -10,14 +10,28 @@ export class ExerciciosView extends View {
       <div class="exercicios">
         <div class="title">
           <h1>EXERCÍCIOS</h1>
-          <!--<i class="fas fa-weight-hanging"></i>-->
-          <button>
-            <i class="fas fa-plus"></i>
-            Incluir
-          </button>
-
+          <i class="fas fa-plus-circle" title="Incluir Exercício"></i>
         </div>
+
         <div class="container">
+          <div class="exercicio">
+            <h2>Pullover</h2>
+            <img src="http://localhost:3333/exercises/5e48ad343328-mulher-exercicio-pullover.jpg" alt="pullover">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis voluptatem</p>
+            <div class="actions">
+              <i class="fas fa-edit" title="Alterar"></i>
+              <i class="fas fa-trash-alt" title="Excluir"></i>
+            </div>
+          </div>
+          <div class="exercicio">
+            <h2>Supino</h2>
+            <img src="http://localhost:3333/exercises/87a7baa3967e-supino.jpeg" alt="supino">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis voluptatem apenas um teste para ver como fica na tela lorem ipsum</p>
+            <div class="actions">
+            <i class="fas fa-edit" title="Alterar"></i>
+            <i class="fas fa-trash-alt" title="Excluir"></i>
+            </div>
+          </div>
           <div class="exercicio">
             <h2>Pullover</h2>
             <img src="http://localhost:3333/exercises/5e48ad343328-mulher-exercicio-pullover.jpg" alt="pullover">
